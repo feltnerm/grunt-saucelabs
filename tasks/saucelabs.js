6aed12289c1f33cf8c290c152b0b1fa525c89559
Mark Feltner <feltner.mj@gmail.com>
2013-06-20 15:15:24 -0500

    fix: Log the debug.

--- tasks/saucelabs.js
+++ tasks/saucelabs.js
@@ -332,7 +332,7 @@ module.exports = function(grunt) {
     grunt.verbose.writeln("[%s] Starting mocha tests for page", cfg.prefix);
     driver.waitForCondition("window.chocoReady", testReadyTimeout, testInterval, function (err) {
       if (err) {
-        grunt.debug.writeln("[%s] Unable to find `mocha.chocoReady` object.", cfg.prefix);
+        grunt.log.debug("[%s] Unable to find `mocha.chocoReady` object.", cfg.prefix);
         callback(false);
         return;
       } else {
